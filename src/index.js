c082a33258fb4f194bd9951bd522754455104510
wjdru <wjdrudtn0225@naver.com>
2018-04-18 21:23:42 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import InchToCm from './InchToCm.js'
+
+ReactDOM.render(
+    <div><InchToCm/></div>,
+    document.getElementById('container')
+)
